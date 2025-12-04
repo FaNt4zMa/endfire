4f6d9ae11a25ed362b6be84235ea5b3488e3e646
Fantaz <nathanyel916d@gmail.com>
2025-12-04 17:46:21 -0500

    Initial commit

---
+++ src/main/java/de/fantaz/endfire/Endfire.java
@@ -0,0 +1,23 @@
+package de.fantaz.endfire;
+
+import com.mojang.logging.LogUtils;
+import de.tomalbrc.filament.api.FilamentLoader;
+import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+
+public class Endfire implements ModInitializer {
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    @Override
+    public void onInitialize() {
+        try {
+            FilamentLoader.loadBlocks("endfire");
+            PolymerResourcePackUtils.addModAssets("endfire");
+            PolymerResourcePackUtils.markAsRequired();
+        } catch (Exception e) {
+            LOGGER.error("Could not load some files!");
+            e.printStackTrace();
+        }
+    }
+}
